25e60f7c4b8eadf14452c9c94fa75dfbdc3de2de
PeterM <peter.mahady1@gmail.com>
2018-05-08 21:16:56 +0200

    Initial Commit

---
+++ AspNetCoreNlogLoggly/AspNetCoreNlogLoggly/Startup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using NLog.Web;
+using AspNetCoreNlogLoggly.Services;
+
+namespace AspNetCoreNlogLoggly
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets<Startup>();
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.ConfigureLoggly(Configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            loggerFactory.AddNLog();
+            env.ConfigureNLog("nlog.config");
+            app.UseMvc();
+        }
+    }
+}
